80826c1cb3bf665c3bd81130277ac5728a6070e9
inomajon <amonijon6@aol.com>
2015-12-08 22:08:49 +0000

    Style Changes

--- gradingapp.js
+++ gradingapp.js
@@ -6,5 +6,19 @@ function giveGrade() {
   alert("hey!");
 
 //Input your code below:
-
-  }
+if(grade<=100 && grade>=90) {
+  alert("A");
+}
+else if(grade<=89 && grade>=80) {
+  alert("B");
+}
+if(grade<=79 && grade>=70) {
+  alert("C");
+}
+else if(grade<=69 && grade>=65) {
+  alert("D");
+}
+if(grade<=64) {
+  alert("F");
+}
+}
